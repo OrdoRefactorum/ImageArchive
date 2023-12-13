92de2408366ee3302b47c18afcb2e5214fa49709
OrdoRefactorum <106595075+OrdoRefactorum@users.noreply.github.com>
2023-12-13 14:37:04 +0100

    changed id generation to use IDENTITY method
    as otherwise big gaps in the id would happen.

--- src/main/java/ch/mse/image_archive/model/Image.java
+++ src/main/java/ch/mse/image_archive/model/Image.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 @Entity
 public class Image {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String name;
